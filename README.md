49030fb7ddd0612753fdda4f463b076dfd364392
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-02-16 23:10:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Windows Forms Assignment: Student Registration For 
+
+This repository contains my submission for the **Windows Forms Assignment** as part of my university coursework. The assignment requires building a **Student Registration Form** using **C# Windows Forms**.  
+
+---
+
+
+## **Screenshot**  
+![Application Preview](KhaledAbdulhakeemSaeedSaifAlshibani_WinFormsAssignment.png)  
+
+---  
+
+## **Assignment PDF**  
+For assignment details, refer to the PDF:  
+[Windows Forms Assignment - Student Registration Form](<Windows Forms Assignment_ Student Registration Form.pdf>)  
+
+---
+
+## **Source code**
+The complete source code is in the [`src`](./src/) directory.  
---
+++ src/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            country.SelectedItem = "Yemen";
+            email.Validating += Validate_Email;
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorPicker.ShowDialog();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Validate_Email(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string regexPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            if (!Regex.IsMatch(email.Text, regexPattern))
+            {
+                MessageBox.Show(
+                    "Invalid email format!",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                e.Cancel = true;
+            }
+        }
+
+        private void email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdmit_Click(object sender, EventArgs e)
+        {
+            string fullName = full_name.Text.ToString().Trim();
+            string emailAddress = email.Text.ToString().Trim();
+            string gender = gender_male.Checked ? "Male" : gender_female.Checked ? "Female" : "";
+
+            string favoriteColor = colorPicker.Color.Name;
+
+            string birthDate = birthdate.Value.ToString("dd/MM/yyyy");
+            string chosenCountry = country.SelectedItem?.ToString() ?? "Not selected";
+            preview.Text = $"Name: {fullName},\n" +
+                $"Email: {emailAddress},\n" +
+                $"Gender: {gender},\n" +
+                $"Favorite Color: {favoriteColor},\n" +
+                $"Birth Date: {birthDate},\n" +
+                $"Country: {chosenCountry}";
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gender_female_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
